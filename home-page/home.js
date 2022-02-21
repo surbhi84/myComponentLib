e5b5c4620cbe959a3310d77ebce948f3fa7f0364
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2022-02-22 01:14:43 +0530

    add: comments in js & removed unused media query

--- home-page/home.js
+++ home-page/home.js
@@ -5,14 +5,16 @@ if (menuBtn.style.display == "none") column.classList.remove("display-none");
 else column.classList.add("display-none");
 
 menuBtn.addEventListener("click", viewColumn);
+
+// onresize event fires on resize of the window so whenever the innerwidth i.e available space in the window is greater then 768px the colum will appear and vice versa.
 window.onresize = () => {
-  // console.log(menuBtn.style);
+  console.log(window.innerWidth);
   if (window.innerWidth >= 768) column.classList.remove("display-none");
   else {
     column.classList.add("display-none");
   }
 };
-
+// the same function will be fired on onload as onresize
 window.onload = window.onresize;
 
 function viewColumn() {
