a43b63a55f02c03a052943b32127b024e1ac242f
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2022-02-22 23:00:46 +0530

    fix: onresize for mobile

--- home-page/home.js
+++ home-page/home.js
@@ -1,21 +1,35 @@
 let menuBtn = document.querySelector(".col-1-btn");
 let column = document.querySelector(".col-1");
 
+let initialScreenSize = -1;
+
 if (menuBtn.style.display == "none") column.classList.remove("display-none");
 else column.classList.add("display-none");
 
 menuBtn.addEventListener("click", viewColumn);
 
-// onresize event fires on resize of the window so whenever the innerwidth i.e available space in the window is greater then 768px the colum will appear and vice versa.
-window.onresize = () => {
-  console.log(window.innerWidth);
+// onload fires on loading of the document
+window.onload = () => {
+  initialScreenSize = window.innerWidth;
   if (window.innerWidth >= 768) column.classList.remove("display-none");
   else {
     column.classList.add("display-none");
   }
 };
-// the same function will be fired on onload as onresize
-window.onload = window.onresize;
+
+// onresize event fires on resize of the window so whenever the innerwidth i.e available space in the window is greater then 768px the colum will appear and vice versa.
+window.onresize = () => {
+  if (
+    (initialScreenSize <= 786 && window.innerWidth >= 786) ||
+    (initialScreenSize >= 786 && window.innerWidth <= 786)
+  ) {
+    if (window.innerWidth >= 768) column.classList.remove("display-none");
+    else {
+      column.classList.add("display-none");
+    }
+    initialScreenSize = window.innerWidth;
+  }
+};
 
 function viewColumn() {
   if (column.classList.contains("display-none"))
