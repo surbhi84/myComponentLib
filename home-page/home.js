23f4238bc18d37d8104930fbfbb3ef08c8958b32
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2022-02-27 01:34:05 +0530

    refactor: added debouncing to resize

--- home-page/home.js
+++ home-page/home.js
@@ -3,6 +3,35 @@ let column = document.querySelector(".col-1");
 
 let initialScreenSize = -1;
 
+// creating debounce function which will return a debounced function
+function debounce(toBeDebounced, delay) {
+  let timer;
+  return function (...x) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      toBeDebounced(...x);
+    }, delay);
+  };
+}
+
+// a function which checks if the resize should bring the column back in screen or let it stay hidden
+function columnHideOnresize() {
+  console.log(window.innerWidth);
+  if (
+    (initialScreenSize <= 786 && window.innerWidth >= 786) ||
+    (initialScreenSize >= 786 && window.innerWidth <= 786)
+  ) {
+    if (window.innerWidth >= 768) column.classList.remove("display-none");
+    else {
+      column.classList.add("display-none");
+    }
+    initialScreenSize = window.innerWidth;
+  }
+}
+
+// creating a debounced version of the above function with our debounced function
+let debouncedColumnHideOnresize = debounce(columnHideOnresize, 1000);
+
 if (menuBtn.style.display == "none") column.classList.remove("display-none");
 else column.classList.add("display-none");
 
@@ -19,16 +48,7 @@ window.onload = () => {
 
 // onresize event fires on resize of the window so whenever the innerwidth i.e available space in the window is greater then 768px the colum will appear and vice versa.
 window.onresize = () => {
-  if (
-    (initialScreenSize <= 786 && window.innerWidth >= 786) ||
-    (initialScreenSize >= 786 && window.innerWidth <= 786)
-  ) {
-    if (window.innerWidth >= 768) column.classList.remove("display-none");
-    else {
-      column.classList.add("display-none");
-    }
-    initialScreenSize = window.innerWidth;
-  }
+  debouncedColumnHideOnresize();
 };
 
 function viewColumn() {
